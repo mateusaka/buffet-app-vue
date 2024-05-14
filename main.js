974bc5802afc14d7934132b1f4cb0b66da60db70
Mateus Correia <correia.mateus@hotmail.com>
2024-05-14 11:58:34 -0300

    Adicionado consulta de disponibilidade

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const app = Vue.createApp({
             currentBuffet: {},
             events: [],
             searchQuery: '',
-            searchResult: []
+            searchResult: [],
+            availQuantity: 0,
+            availDate: null
         }
     },
 
@@ -34,6 +36,8 @@ const app = Vue.createApp({
         },
 
         async search(query) {
+            if (query == '') return
+
             try {
                 const response = await fetch(`http://127.0.0.1:3000/api/v1/buffets/search?name=${query}`)
                 const data = await response.json()
@@ -41,6 +45,34 @@ const app = Vue.createApp({
             } catch (error) {
                 console.log(error)
             }
+        },
+
+        async availability(id, date, quantity) {
+            const element = document.getElementById(`custom-alert-${id}`)
+            element.classList.remove("alert-success", "alert-danger")            
+
+            try {
+                const response = await fetch(`http://127.0.0.1:3000/api/v1/events/availability?id=${id}&date=${date}&quantity=${quantity}`)
+                const data = await response.json()
+
+                if (data.error) {
+                    element.classList.add("alert-danger")
+                    element.innerHTML = `<strong>Erro</strong> ${data.error}`
+                }
+                else {
+                    element.classList.add("alert-success")
+                    element.innerHTML = `<strong>Sucesso</strong> Evento disponível! Valor: $ ${data.value}`
+                }
+
+                element.classList.remove("d-none")
+            } catch (error) {
+                console.log(error)
+            }
+        },
+
+        closeAlert(id) {
+            const element = document.getElementById(`custom-alert-${id}`)
+            element.classList.add("d-none")
         }
     }
 })
